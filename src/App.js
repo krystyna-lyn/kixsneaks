4fa733cbd5fce647d011fa1a9436d6048e9a9c6f
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-08-23 21:01:39 +0200

    add header, img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+import 'macro-css';
+
 
 
 function App() {
   return (
-    <div className="App">
-      Kixsneaks shop
-      <h1>welcome to <span>your </span>shop</h1>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className='d-flex align-center'>
+          <img width={40} height={40} src="./img/logo.png" alt="logo" />
+          <div>
+            <h3 className='text-uppercase'>KixsSneaks</h3>
+            <p>Best sneakers ever</p>
+          </div>
+        </div>
+        <ul className='d-flex'>
+          <li className='mr-30'>
+            <img src="./img/cart.svg" alt="cart" />
+            <span>120€</span>
+          </li>
+          <li>
+            <img src="./img/user.svg" alt="user" />
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40">
+        <h1>All sneakers</h1>
+      </div>
     </div>
   );
 }
