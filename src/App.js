154c91327ca0fadd301595a49997df2e2f94859f
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-09-05 19:53:45 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className='drawer'>
-          <h2 className='mb-30'>Cart</h2>
-
+          <h2 className='d-flex justify-between mb-30'>Cart
+            <img className='removeBtn cu-p' src="./img/btn-remove.svg" alt="remove" />
+          </h2>
           <div className='items'>
             <div className='cartItem d-flex align-center mb-20'>
               <div
@@ -38,19 +39,23 @@ function App() {
 
           </div>
 
-
-          <ul className='cartTotalBlock'>
-            <li>
-              <span>Total</span>
-              <div></div>
-              <b>250€</b>
-            </li>
-            <li>
-              <span>Tax 5%</span>
-              <div></div>
-              <b>12,5€</b>
-            </li>
-          </ul>
+          <div className='cartTotalBlock'>
+            <ul>
+              <li>
+                <span>Total</span>
+                <div></div>
+                <b>250€</b>
+              </li>
+              <li>
+                <span>Tax 5%</span>
+                <div></div>
+                <b>12,5€</b>
+              </li>
+            </ul>
+            <button className='greenButton'>Check out
+              <img src='/img/arrow.svg' alt='Arrow' />
+            </button>
+          </div>
         </div>
 
       </div>
