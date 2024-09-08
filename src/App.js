b6d016051ed126820ec05462fcbfcb8069d6a29e
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-09-08 11:21:59 +0100

    created constants

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import 'macro-css';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 import Card from './components/Card';
+import { data } from '../src/constants'
 
 
 function App() {
-
-  const arr = [
-    { id: 1, title: 'Card 1', imgUrl: './img/sneakers/1.jpg', price: 100 },
-    { id: 2, title: 'Card 2', imgUrl: './img/sneakers/2.jpg', price: 200 },
-    { id: 3, title: 'Card 3', imgUrl: './img/sneakers/3.jpg', price: 300 },
-    { id: 4, title: 'Card 4', imgUrl: './img/sneakers/4.jpg', price: 500 },
-  ]
 
   return (
     <div className="wrapper clear">
@@ -29,7 +23,7 @@ function App() {
         <div className="sneakers d-flex justify-between flex-wrap">
 
           {
-            arr.map(obj => {
+            data.map(obj => {
               return (
                 <Card
                   key={obj.id}
---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const data = [
+    { id: 1, title: 'Card 1', imgUrl: './img/sneakers/1.jpg', price: 100 },
+    { id: 2, title: 'Card 2', imgUrl: './img/sneakers/2.jpg', price: 200 },
+    { id: 3, title: 'Card 3', imgUrl: './img/sneakers/3.jpg', price: 300 },
+    { id: 4, title: 'Card 4', imgUrl: './img/sneakers/4.jpg', price: 500 },
+]
