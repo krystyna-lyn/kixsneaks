52c86273e378bb3cbdc396a5a187ffc65d934fb6
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-09-26 12:53:10 +0100

    update app.js, favorite component

--- src/components/pages/Favorites.jsx
+++ src/components/pages/Favorites.jsx
@@ -1,10 +1,12 @@
 import Card from "../Card";
 
-function Favorites({ favorite, addToCart }) {
+function Favorites({ favorite, addToCart, onAddToFavorite }) {
     return (
-        <div>
-            <h1 className="text-center">Favorites</h1>
 
+        <div className="content p-40">
+            <div className='d-flex align-center mb-40 justify-between'>
+                <h1 className="text-center">Favorite</h1>
+            </div>
             <div className="sneakers d-flex justify-between flex-wrap">
 
                 {favorite
@@ -16,7 +18,9 @@ function Favorites({ favorite, addToCart }) {
                                 title={item.title}
                                 imgUrl={item.imgUrl}
                                 price={item.price}
-                                addToCart={() => console.log('added')}
+                                addToCart={(obj) => addToCart}
+                                addFavorite={(obj) => onAddToFavorite(obj)}
+                                favorited={true}
                                 onPlus={(obj) => addToCart(obj)}
                             />
                         )
