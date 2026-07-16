96e67ebfa3800faee377acd8d37fb56c67aa35de
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2026-07-16 13:52:14 +0100

    Update Favorites.jsx

--- src/components/pages/Favorites.jsx
+++ src/components/pages/Favorites.jsx
@@ -2,38 +2,31 @@ import { useContext } from "react";
 import Card from "../Card";
 import AppContext from "../../context";
 
+
 function Favorites({ addToCart }) {
     const { favorite, onAddToFavorite } = useContext(AppContext);
 
     return (
-
         <div className="content p-40">
-            <div className='d-flex align-center mb-40 justify-between'>
+            <div className="d-flex align-center mb-40 justify-between">
                 <h1 className="text-center">Favorite</h1>
             </div>
-            <div className="sneakers d-flex justify-between flex-wrap">
 
-                {favorite
-                    .map(item => {
-                        return (
-                            <Card
-                                key={item.id}
-                                id={item.id}
-                                title={item.title}
-                                imgUrl={item.imgUrl}
-                                price={item.price}
-                                addToCart={(obj) => addToCart}
-                                addFavorite={(obj) => onAddToFavorite(obj)}
-                                favorited={true}
-                                onPlus={(obj) => addToCart(obj)}
-                            />
-                        )
-                    })
-                }
+            <div className="sneakers d-flex justify-between flex-wrap">
+                {favorite.map((item) => (
+                    <Card
+                        key={item.id}
+                        id={item.productId}
+                        title={item.title}
+                        imgUrl={item.imgUrl}
+                        price={item.price}
+                        addFavorite={onAddToFavorite}
+                        onPlus={addToCart}
+                    />
+                ))}
             </div>
         </div>
     );
-
 }
 
 export default Favorites;
